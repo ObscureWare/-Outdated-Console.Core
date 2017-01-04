551085b1dcb9a4b4bd965b35d6ed2a0cdf7bbd67
Sebastian-Gruchacz <sebgruch@gmail.com>
2017-01-04 08:53:06 +0100

    Namespace normalization.

--- src/ObscureWare.Console.Tests/Properties/AssemblyInfo.cs
+++ src/ObscureWare.Console.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ObscureWare.Console.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Sebastian Gruchacz 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- src/ObscureWare.Console/Properties/AssemblyInfo.cs
+++ src/ObscureWare.Console/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ObscureWare.Console.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Obscureware Solutions")]
+[assembly: AssemblyCompany("ObscureWare Solutions")]
 [assembly: AssemblyProduct("ObscureWare.Console.Core")]
 [assembly: AssemblyCopyright("Copyright © Sebastian Gruchacz 2015-2017")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.6.*")]
-[assembly: AssemblyFileVersion("0.2.6.0")]
+[assembly: AssemblyVersion("0.2.7.*")]
+[assembly: AssemblyFileVersion("0.2.7.0")]
